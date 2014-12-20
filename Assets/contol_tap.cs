e305d726d00d559c0461f8d59f1e2d61b2952cbe
slava <slonofanya@rambler.ru>
2014-12-20 20:30:31 +0200

    Player move Fixed ghost script

---
+++ Assets/contol_tap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class contol_tap : MonoBehaviour {
+
+	public float move_speed = 1F;
+
+	private Vector3 tap_pos;
+	private GameObject[] players;
+	private int center;
+
+	void Start() {
+		center = 360;
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			tap_pos = Input.mousePosition;
+
+			Vector3 direction;
+
+			if (tap_pos.x < center)
+				direction = Vector3.up;
+			else
+				direction = -Vector3.up;
+
+			foreach (GameObject player in players) {
+				player.rigidbody2D.velocity = Vector3.zero;
+				player.transform.position += direction * move_speed;
+			}
+		}
+	}
+}
